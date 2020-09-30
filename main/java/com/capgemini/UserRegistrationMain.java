66fff4bbf8c161183bbd9c9835231a8ead64aeb6
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-01 01:36:10 +0530

    [Shreya]Added:Lastname

--- main/java/com/capgemini/UserRegistrationMain.java
+++ main/java/com/capgemini/UserRegistrationMain.java
@@ -1,26 +1,44 @@
 package com.capgemini;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
 
 public class UserRegistrationMain {
-	public void firstNameValidator(String firstName) {
+	Scanner sc = new Scanner(System.in);
+	public void firstNameValidator() {
+		System.out.println("Enter your first name");
+		String firstName = sc.next();
 		Pattern pattern = Pattern.compile("^([A-Z])[a-zA-Z]{2,}$");
 		Matcher matcher = pattern.matcher(firstName);
 		Boolean matchFound = matcher.find();
-        if(matchFound)
-        	System.out.println("First Name is Validated");
-        else
-        	System.out.println("Please check your input");
-        
+		if (matchFound) {
+			System.out.println("First Name is Validated");
+		} else {
+			System.out.println("Please check your input");
+		}
 	}
+	public void lastNameValidator() {
+		System.out.println("Enter your last name");
+		String lastName = sc.next();
+		Pattern pattern = Pattern.compile("^([A-Z])[a-zA-Z]{2,}$");
+		Matcher matcher = pattern.matcher(lastName);
+		Boolean matchFound = matcher.find();
+		if (matchFound) {
+			System.out.println("Last Name is Validated");
+		} else {
+			System.out.println("Please check your input");
+		}
+	}
+ 
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Page");
-        System.out.println("Enter your first name :");
-        Scanner sc = new Scanner(System.in);
-        UserRegistrationMain u = new UserRegistrationMain();
-        u.firstNameValidator(sc.next());
+		
+		UserRegistrationMain u = new UserRegistrationMain();
+		u.firstNameValidator();
+		u.lastNameValidator();
+
 	}
 
 }
