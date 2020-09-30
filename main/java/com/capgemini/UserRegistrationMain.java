13271a2b74401dfa4921b690e2e749e853a37229
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-01 01:48:12 +0530

    [Shreya]Added:Email

--- main/java/com/capgemini/UserRegistrationMain.java
+++ main/java/com/capgemini/UserRegistrationMain.java
@@ -30,6 +30,18 @@ public class UserRegistrationMain {
 			System.out.println("Please check your input");
 		}
 	}
+	public void emailValidator() {
+		System.out.println("Enter your email");
+		String email = sc.next();
+		Pattern pattern = Pattern.compile("^([a-z]){1,}([.+_-]){0,1}[0-9a-z]*(@){1}([0-9a-z]*)(.([a-z]){2,}){1}(.[a-z][a-z])?$");
+		Matcher matcher = pattern.matcher(email);
+		Boolean matchFound = matcher.find();
+		if (matchFound) {
+			System.out.println("Email is Validated");
+		} else {
+			System.out.println("Please check your input");
+		}
+	}
  
 
 	public static void main(String[] args) {
@@ -38,6 +50,7 @@ public class UserRegistrationMain {
 		UserRegistrationMain u = new UserRegistrationMain();
 		u.firstNameValidator();
 		u.lastNameValidator();
+		u.emailValidator();
 
 	}
 
